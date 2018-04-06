f149376f585120b8c689d8b890c9911a329fa837
Prashan Pudasaini <prashanp16@gmail.com>
2018-04-06 13:50:17 -0500

    Add files via upload

--- calculator.cpp
+++ calculator.cpp
@@ -1,102 +1,290 @@
+﻿/*FILE: calculator.cpp
+@Project 2
+@file-location: /export/home/CSCI 330/
+@Date: April 6, 2018
+@Date Handed - In: April 13, 2018
+@Description: This is a calculator program. This program can be used for addition, substraction, multiplication, division, modulo,*/ 
+/*check if a given number is prime, odd, or even.*/
+
 #include <iostream>
+#include <limits>
+#include <math.h>
 using namespace std;
 
-int add(double a, double b)
+enum Options { NIL, ADD, SUB, MULT, DIV, MOD, PRIME, EVEN, ODD, HELP, QUIT };
+
+void welcomeMessage()
 {
-	double sum;
+	cout << "\t┌───────────────────────────────────────┐\n"
+		"\t│               Calculator              │\n"
+		"\t├───────────────────────────────────────┤\n"
+		"\t│ Welcome to the basic calculator       │\n"
+		"\t│ program. This allows you to do        │\n"
+		"\t│ various operations using two operands │\n"
+		"\t│ or determine if an integer is prime,  │\n"
+		"\t│ odd, or even.                         │\n"
+		"\t│                                       │\n"
+		"\t│                     Synth Dolphins,   │\n"
+		"\t│                            April 2018 │\n"
+		"\t└───────────────────────────────────────┘\n";
+}
+
+void displayMenu() 
+{
+	cout << "\t\t┌──────────────────────┐\n"
+		"\t\t│         Menu         │\n"
+		"\t\t├────┬─────────────────┤\n"
+		"\t\t│ 1  │ Addition        │\n"
+		"\t\t│ 2  │ Subtraction     │\n"
+		"\t\t│ 3  │ Multiplication  │\n"
+		"\t\t│ 4  │ Division        │\n"
+		"\t\t│ 5  │ Modulo          │\n"
+		"\t\t│ 6  │ Check if Prime  │\n"
+		"\t\t│ 7  │ Check if Even   │\n"
+		"\t\t│ 8  │ Check if Odd    │\n"
+		"\t\t│ 9  │ Get Help        │\n"
+		"\t\t│ 10 │ Quit            │\n"
+		"\t\t└────┴─────────────────┘\n";
+}
+
+void getHelp() 
+{
+	cout << "   ┌───────────────────────────────────────────────────┐\n"
+		"   │                      Help Menu                    │\n"
+		"   ├───────────────────────────────────────────────────┤\n"
+		"   │ To use this program, simply enter in a number     │\n"
+		"   │ corresponding to the command you wish to enter.   │\n"
+		"   │                                                   │\n"
+		"   │ Commands 1 through 5 all correspond to            │\n"
+		"   │ arithmetic operations, and require the user to    │\n"
+		"   │ enter two valid numbers. Entering anything but a  │\n"
+		"   │ number will result in another prompt asking you   │\n"
+		"   │ to enter a correct value.                         │\n"
+		"   │                                                   │\n"
+		"   │ Commands 6 through 7 are all used to determine if │\n"
+		"   │ an input integer fulfills a certain condition. In │\n"
+		"   │ this case, it determines if it is prime, even, or │\n"
+		"   │ or odd.                                           │\n"
+		"   │                                                   │\n"
+		"   │ When you are done using the calculator, you can   │\n"
+		"   │ use the last option, 10, to quit the program.     │\n"
+		"   └───────────────────────────────────────────────────┘\n";
+}
+
+void quitMessage() 
+{
+	cout << "\t\t┌──────────────────────┐\n"
+		"\t\t│      Good-bye!       │\n"
+		"\t\t└──────────────────────┘\n";
+}
+
+float getInput() 
+{
+	int tries = 0;
+	bool inputFailed;
+	float input;
+	do
+	{
+		if (tries++ > 0)
+			cout << "Input entered was invalid! Please enter another." << endl;
+		cin >> input;
+		inputFailed = cin.fail();
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	} while (inputFailed);
+	return input;
+}
+
+/*@param a the first operand
+@param b the second operand
+@pre None
+@post returns the sum of a and b*/
+float add(double a, double b)
+{
+	float sum;
 	sum = a + b;
-	cout << "Sum of " << a << " + " << b << " is : " << sum << endl;
 	return sum;
 }
-int sub(double a, double b)
+
+/*@param a the first operand
+@param b the second operand
+@pre None
+@post returns the difference of a and b*/
+float sub(double a, double b) //Prashan
 {
 	double difference;
 	difference = a - b;
-	cout << "Difference of " << a << " - " << b << " is : " << difference << endl;
 	return difference;
 }
-int mult(double a, double b)
+
+/*@param a the first operand
+@param b the second operand
+@pre None
+@post returns the product of a and b*/
+float mult(double a, double b) //Prashan
 {
 	double product;
 	product = a * b;
-	cout << "Product of " << a << " * " << b << " is : " << product << endl;
 	return product;
 }
-int divide(double m, double n)
+
+/*@param a the first operanr
+@param b the second operand
+@pre None
+@post returns the quotient of a and b*/
+float divide(double m, double n) //Prashan
 {
 	float division;
-	division = (float) m / n;
-	cout << "Quotient of " << m << " / " << n << " is : " << division << endl;
+	division = (float)m / n;
 	return division;
 }
 
-bool oddOreven(int x)
+/*@param a the first operanr
+@param b the second operand
+@pre None
+@post returns the modulo of a and b*/
+float mod(double a, double b) //Prashan
 {
-	int oddEvenResult = 0;
+	return fmod(a, b);
+}
 
-	if(x % 2 == 0){
-		cout << "The number is even.";
+/*@param x the operand to check for odd
+@pre None
+@post returns whether the operand is odd or even*/
+bool isOdd(int x) //Prashan
+{
+	if (x % 2 == 0) {
+		return false;
 	}
 
 	else {
-		cout << "The number is odd.";
+		return true;
 	}
-	return oddEvenResult;
 }
 
-bool primeCheck(int x, int i) {
-	int primeResult = 0;
-	bool isPrime = true;
+/*@param x the operand to check for prime
+@pre None
+@post returns whether the operand is prime. True if prime, false if not*/
+bool isPrime(int x)
+{
+	bool prime = true;
 
-	for (i = 2; i <= x / 2; ++i)
+	for (int i = 2; i <= x / 2; ++i)
 	{
 		if (x % i == 0)
 		{
-			isPrime = false;
+			prime = false;
 			break;
 		}
 	}
-	if (isPrime) {
-		cout << "This is a prime number";
-	}
-	else {
-		cout << "This is not a prime number";
-	}
-	return primeResult;
+	return prime;
 }
 
-void main()
+int main()
 {
-	double x, y;
-	cout << "Please enter first no: ";
-	cin >> x;
-	cout << "Please enter second no: ";
-	cin >> y;
-	cout << "Please press + for addition" << endl;
-	cout << "Please press - for subtraction" <<endl;
-	cout << "Please press * for multiplication" <<endl;
-	cout << "Please press / for division" <<endl;
-	cout << "Please press 1 to check odd or even for x." << endl;
-	cout << "Please press 2 to check x for prime." << endl;
-	char choice;
-	cout << "Please enter your choice of operation: ";
-	cin >> choice;
-	if (choice == '+')
-		add(x, y);
-	else if (choice == '-')
-		sub(x, y);
-	else if (choice == '*')
-		mult(x, y);
-	else if (choice == '/')
-		divide(x, y);
-	else if (choice == '1')
-		oddOreven(x);
-	else if (choice == '2')
-		primeCheck(x, y);
+	welcomeMessage();
+	int option;
+
+	do
+	{
+		displayMenu();
+		cout << "Please enter an option: ";
+		cin >> option;
+		while (option <= NIL || option > QUIT)
+		{
+			cout << "Invalid! Please enter another option: ";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cin >> option;
+		}
+		if (option <= MOD)
+		{
+			cout << "Please enter the first operand." << endl;
+			double num1 = getInput();
+			cout << "Please enter the second operand." << endl;
+			double num2 = getInput();
+			double result;
+			if (option == ADD) 
+			{
+				result = add(num1, num2);
+				cout << num1 << " + " << num2 << " = " << result << endl;
+			}
+			else if (option == SUB)
+			{
+				result = sub(num1, num2);
+				cout << num1 << " - " << num2 << " = " << result << endl;
+			}
+			else if (option == MULT) 
+			{
+				result = mult(num1, num2);
+				cout << num1 << " * " << num2 << " = " << result << endl;
+			}
+			else if (option == DIV)
+			{
+				result = divide(num1, num2);
+				cout << num1 << " / " << num2 << " = " << result << endl;
+			}
+			else if (option == MOD)
+			{
+				result = mod(num1, num2);
+				cout << num1 << " % " << num2 << " = " << result << endl;
+			}
+		}
+		else if (option <= ODD)
+		{
+			cout << "Plase enter an integer." << endl;
+			float inputFloat = getInput();
+			int num1 = (int)inputFloat;
+			while (num1 != inputFloat)
+			{
+				cout << "Please only enter an integer!" << endl;
+				inputFloat = getInput();
+				num1 = (int)inputFloat;
+			}
+			bool result;
+			if (option == PRIME)
+			{
+				result = isPrime(num1);
+				if (result)
+					cout << num1 << " is prime!" << endl;
+				else
+					cout << num1 << " is not prime!" << endl;
+			}
+			else if (option == EVEN)
+			{
+				result = !isOdd(num1);
+				if (result)
+					cout << num1 << " is even!" << endl;
+				else
+					cout << num1 << " is not even!" << endl;
+			}
+			else if (option == ODD)
+			{
+				result = isOdd(num1);
+				if (result)
+					cout << num1 << " is odd!" << endl;
+				else
+					cout << num1 << " is not odd!" << endl;
+			}
+		}
+		else if (option == HELP)
+		{
+			getHelp();
+		}
+		else if (option == QUIT)
+		{
+			quitMessage();
+		}
+		else
+		{
+			cout << "ERROR! INVALID OPTION!" << endl;
+		}
+	} while (option != QUIT);
+
+	return 0;
 
 	cin.get();
-	cin.clear();
 	cin.ignore();
-
+	cin.clear();
 }
 
